080ee3e41a04b3405050061256f4d1e4c454b1f6
Katekko <joaovixgon@gmail.com>
2020-09-15 17:39:04 -0300

    Fix lint

--- lib/commands/create/screen/screen.dart
+++ lib/commands/create/screen/screen.dart
@@ -13,7 +13,6 @@ import 'samples/get_screen.dart';
 class CreateScreenCommand extends Command with CreateMixin {
   @override
   Future<void> execute() async {
-    // get create screen:"Home"
     var split = GetCli.arguments[1].split(':');
     var hasScreenName = split.length == 2;
     if (hasScreenName) {
@@ -55,38 +54,21 @@ class CreateScreenCommand extends Command with CreateMixin {
         controllerImport:
             './../../../../presentation/${name.snakeCase}/controllers/${name.snakeCase}.controller.dart',
       ).create();
+
+      await addExport(
+        path:
+            'lib/infrastructure/navigation/bindings/controllers/controllers_bindings.dart',
+        line: '''export '${name.snakeCase}.controller.binding.dart';''',
+      );
     } else {
       LogService.error(
         'Declare a name for your screen: get create screen:"Name of your Screen"',
       );
     }
-    // String name = isProject ? 'home' : this.name;
-    // String baseFolder = 'lib/presentation/';
-    // String screenDir = '${name.snakeCase}/${name.snakeCase}.screen.dart';
-    // String controllerDir =
-    //     'presentation/${name.snakeCase}/controllers/${name.snakeCase}.controller.dart';
-    // String bindingDir =
-    //     'lib/infrastructure/navigation/bindings/controllers/${name.snakeCase}.controller.binding.dart';
-
-    // await GetViewSample('$baseFolder/$screenDir', '${name.pascalCase}Screen',
-    //         '${name.pascalCase}Controller', controllerDir)
-    //     .create();
-
-    // await addExport('lib/presentation/screens.dart', 'export \'$screenDir\';');
-    // await BindingSample(bindingDir, name, '${name.pascalCase}ControllerBinding',
-    //         controllerDir)
-    //     .create();
-    // await addExport(
-    //     'lib/infrastructure/navigation/bindings/controllers/controllers_bindings.dart',
-    //     '''export '${name.snakeCase}.controller.binding.dart';''');
-
-    // await ControllerSample('lib/$controllerDir', name).create();
-
-    // await arcAddRoute(name);
   }
 
   @override
-  String get hint => 'Create a screen';
+  String get hint => 'Create a new screen with binding and routes';
 
   @override
   bool validate() => true;

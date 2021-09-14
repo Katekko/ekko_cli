18b63254df2a1f80bae9e0df9cb4364b4c09e1ff
Katekko <joaovixgon@gmail.com>
2021-09-14 17:56:26 -0300

    Criando data baseado nos attributos recebidos

--- lib/commands/create/app/model/samples/data.sample.dart
+++ lib/commands/create/app/model/samples/data.sample.dart
@@ -4,11 +4,36 @@ import 'package:recase/recase.dart';
 class DataSample extends Sample {
   final String _path;
   final String _fileName;
-  DataSample({required String path, required String fileName})
-      : _path = path,
-        _fileName = fileName;
+  final Map<String, String>? _attributs;
+  DataSample({
+    required String path,
+    required String fileName,
+    Map<String, String>? attributs,
+  })  : _path = path,
+        _fileName = fileName,
+        _attributs = attributs;
 
   String get name => _fileName.split('D')[0];
+
+  String get getAttributs {
+    var str = '';
+    if (_attributs != null) {
+      for (final key in _attributs!.keys) {
+        str += '''\n  final $key ${_attributs![key]};''';
+      }
+    }
+    return str;
+  }
+
+  String get getConstructor {
+    var str = '';
+    if (_attributs != null) {
+      for (final key in _attributs!.keys) {
+        str += ', required this.${_attributs![key]}';
+      }
+    }
+    return str;
+  }
 
   @override
   String get content =>
@@ -19,7 +44,8 @@ part '${name.snakeCase}.data.g.dart';
 @JsonSerializable()
 class $_fileName {
   final int id;
-  const $_fileName({required this.id});
+  $getAttributs
+  const $_fileName({required this.id$getConstructor});
 
   factory $_fileName.fromJson(Map<String, dynamic> json) =>
       _\$${_fileName}FromJson(json);

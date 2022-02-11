5b3bf5ce66ce28d16b9884b5500f9b314e5fb80a
Katekko <joaovixgon@gmail.com>
2022-02-11 10:00:37 -0300

    Disabling create model command

--- lib/commands.dart
+++ lib/commands.dart
@@ -1,14 +1,10 @@
 import 'package:ekko_cli/commands/help/help.dart';
 import 'package:ekko_cli/commands/version/version.dart';
 
-import 'commands/create/model/model.dart';
 import 'commands/create/screen/screen.dart';
 
 final commands = {
-  'create': {
-    'screen': () => CreateScreenCommand(),
-    'model': () => CreateModelCommand(),
-  },
+  'create': {'screen': () => CreateScreenCommand()},
   'help': () => HelpCommand(),
   'version': () => VersionCommand(),
 };
